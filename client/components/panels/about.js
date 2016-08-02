0f3627f265a5029b6620734fd588e1a08939b341
pberens <pberens>
2016-08-02 11:53:47 +0200

    Updated text

--- client/components/panels/about.js
+++ client/components/panels/about.js
@@ -9,15 +9,44 @@ module.exports = function (state) {
   }
   return hx`<div>
     <div>
-      Calcium <a href='https://en.wikipedia.org/wiki/Calcium_imaging'>imaging</a> is a dominant technique in modern neuroscience for measuring the activity of large populations of neurons. But the resulting flouresence signal is only an indirect measure of neuronal spiking. How to best identify the timing of individual spiking events from flouresence time courses remains an open problem.
+		Over the last two decades, calcium <a href='https://en.wikipedia.org/wiki/Calcium_imaging'>imaging</a> has become a dominant technique in modern neuroscience for measuring the activity of large populations of neurons. A calcium indicator such as the synthetic dye Oregon green BAPTA-1 or the genetically encoded GCaMP-reporters is used to image a large fraction of cells in a neural tissue. Individual action potentials are associated with a fast rise in fluorescence, followed by a slow decay with a time constant of typically hundreds of milliseconds. Therefore, the measured fluorescence signal is only an indirect measure of neuronal spiking activity. How to best identify the timing of individual spiking events from flouresence time courses remains an open problem.
+    </div>
+	<br>
+    <div>
+		<b>Here is the challenge</b>: We have assembled a collection of datasets of simultaneously measured calcium traces and action potentials (ground truth) for benchmarking algorithms. You can download the data, develop your algorithm in the language of your choice, and submit a file to this website with your results. The leaderboard will rank algorithm performance across datasets. We will close submissions on X.X.2017.
+		
+    </div>
+    
+    <br>
+    <div>
+		<b>What's in it for you</b>: 
+			<ul>
+				<li>You get to advance the field and do a servive to the community.</li>
+				<li>The best three algorithms will receive an award, kindly sponspored by <a href="">Zeiss AG</a>. The winner will receive 1200€, the second place 500€ and the third place 300€.</li>
+				<li>We will compile a review paper detailing the advances made and reviewing the current state in spike inference for calcium imaging. It will feature the top five entries and include you as co-authors. </li>  
+		
     </div>
     <br>
     <div>
-    EXPLAIN MORE ABOUT THE PROBELM
+        See the 'download' and 'submit' tabs to get started. 
+	  
+	  If you have any problems you can open an issue on <a href='https://github.com/codeneuro/spikefinder'>github</a> or come talk to us in the <a href='https://gitter.im/codeneuro/spikefinder'>chatroom</a>!
     </div>
-    <br>
+	
+	<br>
     <div>
-      We have assembled a collection of datasets with ground truth for benchmarking algorithms. You can download the data, develop your algorithm in the language of your choice, and submit a file to this website with your results. The leaderboard will rank algorithm performance across datasets. See the 'download' and 'submit' tabs to get started. If you have any problems you can open an issue on <a href='https://github.com/codeneuro/spikefinder'>github</a> or come talk to us in the <a href='https://gitter.im/codeneuro/spikefinder'>chatroom</a>!
+        <a href="http://www.berenslab.org" target=_blank>Philipp Berens (University of Tübingen)</a>
+		<a href="http://thefreemanlab.com" target=_blank>Jeremy Freeman (Janelia Research Campus)
+		<a href="http://www.bethgelab.org" target=_blank>Matthias Bethge (University of Tübingen)
+		<a href="http://www.jovo.me" target=_blank>Joshua Vogelstein (Johns Hopkins)
+		<a href="" target=_blank>Lucas Theis (Magic Pony Technology / Twitter)
+		
     </div>
+	
+	
+	
+	
+	
+	
   </div>`
 }
--- client/components/panels/download.js
+++ client/components/panels/download.js
@@ -41,18 +41,26 @@ module.exports = function (state) {
 
   return hx`<div>
     <div>
-      Data has been generously provided by LIST NAMES OF LABS.
+      The data provided here consists of simultaneously measured calcium traces and spike trains. They were obtained using different calcium indicators (OGB, GCamp6s), in different neural tissues (V1, retina) using different scanning technologies (AOD, galvo, resonant) and cover therefore quite a range of applications. Importantly, all cells were recorded in "population imaging" mode instead of zooming in on individual cells. For details regarding the dataset, please refer to Theis et al. (2016).
     </div>
-    <br>
+	<br>
+    
+	<div>
+      Data has been generously provided by the <a href="http://www.toliaslab.org" target=_blank>Andreas Tolias lab</a> (Baylor College of Medicine, Houston, USA) and the <a href="http://www.eulerlab.org" target=_blank>Euler lab</a> (University of Tübingen, Germany).
+    </div>
+    <br>   
+
     <div>
+      Ground truth spike trains were obtained by performing single cell electrophysiological recordings simultaneous with the two-photon imaging. Calcium and spiking data has been resampled to a common sampling rate of 100 Hz. We removed any linear trends from the calcium trace using robust regression. 
+	</div>
+    <br>
+
+	
+	<div>
       Each dataset is available as a zip file, and includes DESCRIBE DATA FORMAT, metadata, and code for loading the data in python, and matlab. The code examples are also on <a href='https://github.com/codeneuro/spikefinder-datasets'>github</a>. ANY FURTHER DATA DESCRIPTION Labels are provided only for training data.
     </div>
-    <br>
-    <div>
-      EXPLANATION OF HOW GROUND TRUTH LABELS WERE OBTAINED
-    </div>
-    <br>
-    <div>
+    
+	<div>
       Training datasets (including labels) POINT TO REAL DATA
     </div>
     <br>
@@ -63,5 +71,19 @@ module.exports = function (state) {
     </div>
     <br>
     <div style=${style.list}>${testing()}</div>
+	
+	<br>
+    <div>
+      WE COULD ADD TABLE 1 FROM THE PAPER HERE
+    </div>
+    <br>
+	
+	
+	<div>
+      References: <br>
+	  Theis, Berens, Froudarakis, Reimer, Roman-Roson, Euler, Tolias, Bethge (2016):  Benchmarking Spike Rate Inference in Population Calcium Imaging, Neuron, <a href="http://linkinghub.elsevier.com/retrieve/pii/S0896627316300733">Link</a>
+    </div>
+    <br>
+    
   </div>`
 }
